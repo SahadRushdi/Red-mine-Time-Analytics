0da2d918d7ebf184db33f2450996d94639b40f5e
SahadRushdi <rushdi1823@gmail.com>
2026-01-09 17:40:52 +0530

    refactor: removed invalid holidays

--- lib/redmine_time_analytics/sri_lankan_holidays.rb
+++ lib/redmine_time_analytics/sri_lankan_holidays.rb
@@ -12,7 +12,7 @@ module RedmineTimeAnalytics
 
     # Fixed Mercantile/Bank Holidays
     MERCANTILE_HOLIDAYS = [
-      [1, 14],  # Tamil Thai Pongal Day
+      [1, 15],  # Tamil Thai Pongal Day
       [4, 13],  # Sinhala and Tamil New Year's Eve
       [4, 14]   # Sinhala and Tamil New Year Day
     ].freeze
@@ -22,14 +22,14 @@ module RedmineTimeAnalytics
     # or calculated using lunar calendar library
     POYA_DAYS_2024 = [
       Date.new(2024, 1, 25),  # Duruthu Poya
-      Date.new(2024, 2, 24),  # Navam Poya
-      Date.new(2024, 3, 25),  # Medin Poya
+      Date.new(2024, 2, 23),  # Navam Poya
+      Date.new(2024, 3, 24),  # Medin Poya
       Date.new(2024, 4, 23),  # Bak Poya
       Date.new(2024, 5, 23),  # Vesak Poya
       Date.new(2024, 6, 21),  # Poson Poya
-      Date.new(2024, 7, 21),  # Esala Poya
+      Date.new(2024, 7, 20),  # Esala Poya
       Date.new(2024, 8, 19),  # Nikini Poya
-      Date.new(2024, 9, 18),  # Binara Poya
+      Date.new(2024, 9, 17),  # Binara Poya
       Date.new(2024, 10, 17), # Vap Poya
       Date.new(2024, 11, 15), # Il Poya
       Date.new(2024, 12, 14)  # Unduvap Poya
@@ -38,12 +38,12 @@ module RedmineTimeAnalytics
     POYA_DAYS_2025 = [
       Date.new(2025, 1, 13),  # Duruthu Poya
       Date.new(2025, 2, 12),  # Navam Poya
-      Date.new(2025, 3, 14),  # Medin Poya
-      Date.new(2025, 4, 13),  # Bak Poya
+      Date.new(2025, 3, 13),  # Medin Poya
+      Date.new(2025, 4, 12),  # Bak Poya
       Date.new(2025, 5, 12),  # Vesak Poya
-      Date.new(2025, 6, 11),  # Poson Poya
+      Date.new(2025, 6, 10),  # Poson Poya
       Date.new(2025, 7, 10),  # Esala Poya
-      Date.new(2025, 8, 9),   # Nikini Poya
+      Date.new(2025, 8, 8),   # Nikini Poya
       Date.new(2025, 9, 7),   # Binara Poya
       Date.new(2025, 10, 6),  # Vap Poya
       Date.new(2025, 11, 5),  # Il Poya
@@ -51,38 +51,32 @@ module RedmineTimeAnalytics
     ].freeze
 
     POYA_DAYS_2026 = [
-      Date.new(2026, 1, 2),   # Duruthu Poya
-      Date.new(2026, 1, 31),  # Navam Poya
-      Date.new(2026, 3, 3),   # Medin Poya
+      Date.new(2026, 1, 3),  # Duruthu Poya
+      Date.new(2026, 2, 1),  # Navam Poya
+      Date.new(2026, 3, 2),   # Medin Poya
       Date.new(2026, 4, 1),   # Bak Poya
       Date.new(2026, 5, 1),   # Vesak Poya (overlaps with May Day)
       Date.new(2026, 5, 30),  # Poson Poya
       Date.new(2026, 6, 29),  # Esala Poya
-      Date.new(2026, 7, 28),  # Nikini Poya
+      Date.new(2026, 7, 29),  # Nikini Poya
       Date.new(2026, 8, 27),  # Binara Poya
-      Date.new(2026, 9, 25),  # Vap Poya
+      Date.new(2026, 9, 26),  # Vap Poya
       Date.new(2026, 10, 25), # Il Poya
-      Date.new(2026, 11, 23), # Unduvap Poya
+      Date.new(2026, 11, 24), # Unduvap Poya
       Date.new(2026, 12, 23)  # Unduvap Poya (if two in same month)
     ].freeze
 
     # Islamic Holidays (Ramadan, Hajj) - also vary by lunar calendar
     # These are approximate dates and should be updated annually
     ISLAMIC_HOLIDAYS_2024 = [
-      Date.new(2024, 4, 11),  # Eid al-Fitr (Ramadan Festival)
-      Date.new(2024, 6, 17),  # Eid al-Adha (Hajj Festival)
       Date.new(2024, 9, 16)   # Milad-un-Nabi (Prophet's Birthday)
     ].freeze
 
     ISLAMIC_HOLIDAYS_2025 = [
-      Date.new(2025, 3, 31),  # Eid al-Fitr (Ramadan Festival)
-      Date.new(2025, 6, 7),   # Eid al-Adha (Hajj Festival)
       Date.new(2025, 9, 5)    # Milad-un-Nabi (Prophet's Birthday)
     ].freeze
 
     ISLAMIC_HOLIDAYS_2026 = [
-      Date.new(2026, 3, 20),  # Eid al-Fitr (Ramadan Festival)
-      Date.new(2026, 5, 27),  # Eid al-Adha (Hajj Festival)
       Date.new(2026, 8, 26)   # Milad-un-Nabi (Prophet's Birthday)
     ].freeze
 
